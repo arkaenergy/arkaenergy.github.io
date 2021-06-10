2dedaa3eadff7d000c899aca4e7583c862ff6bb7
Anarghya V <30490462+gitofanarghya@users.noreply.github.com>
2021-06-10 14:43:35 +0530

    removing preloader

--- assets/js/main.js
+++ assets/js/main.js
@@ -150,14 +150,14 @@
 
   /**
    * Preloader
-   */
+   
   let preloader = select('#preloader');
   if (preloader) {
     window.addEventListener('load', () => {
       preloader.remove()
     });
   }
-
+*/
   /**
    * Initiate glightbox 
    */
